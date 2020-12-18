1af7d6b782db1ed6c0f7c8183e43ab26fe439399
hejevero <76176799+hejevero@users.noreply.github.com>
2020-12-18 00:57:29 -0300

    First update

---
+++ pages/estructure/menu.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+	<a class="navbar-brand" href="#">Hejevero</a>
+	<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		<li class="nav-item active">
+			<a class="nav-link" href="#">Inicio<span class="sr-only">(Current)</span></a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Bodegas</a>
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Usuario</a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<a class="dropdown-item" href="#">Informaci&oacute;n</a>
+				<a class="dropdown-item" href="#">Vincular</a>
+				<div class="dropdown-divider"></div>
+				<a class="dropdown-item" href="#">Registrarse</a>
+			</div>
+		</li>
+	</ul>
+	<form class="form-inline my-2 my-lg-0">
+		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Submit</button>
+	</form>
+</div>
+</nav>
