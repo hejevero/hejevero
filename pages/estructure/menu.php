959b60c87ceab161c3f7a058b305642f041546a4
hejevero <76176799+hejevero@users.noreply.github.com>
2020-12-18 16:52:37 -0300

    Update on Work Computer

--- pages/estructure/menu.php
+++ pages/estructure/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-secondary">
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
 </button>
@@ -6,13 +6,23 @@
 	<a class="navbar-brand" href="#">Hejevero</a>
 	<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 		<li class="nav-item active">
-			<a class="nav-link" href="#">Inicio<span class="sr-only">(Current)</span></a>
+			<a class="nav-link" href="#">
+				<img src="./images/icons/house-door.svg" width="25" height="25" class="d-inline-block align-top" style="color: cornflowerblue;" alt="">
+				Inicio
+				<span class="sr-only">(Current)</span>
+			</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" href="#">Bodegas</a>
+			<a class="nav-link" href="#">
+				<img src="./images/icons/plus-square.svg" width="25" height="25" class="d-inline-block align-top" style="color: cornflowerblue;" alt="">
+				Bodegas
+			</a>
 		</li>
 		<li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Usuario</a>
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<img src="./images/icons/file-earmark-person.svg" width="25" height="25" class="d-inline-block align-top" style="color: cornflowerblue;" alt="">
+				Usuario
+			</a>
 			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 				<a class="dropdown-item" href="#">Informaci&oacute;n</a>
 				<a class="dropdown-item" href="#">Vincular</a>
